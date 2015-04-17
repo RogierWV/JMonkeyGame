fab60298b2b6d74579e8b2567e62245789ff8507
HackSlash <markaas@live.nl>
2015-04-17 14:32:21 +0200

    added test files

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -9,7 +9,6 @@ import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.KeyTrigger;
 import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
-import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
